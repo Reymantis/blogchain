f539c995bb2a6bc3ae0ec4803db43d1514c462c1
vagrant <vagrant@homestead>
2025-05-26 21:00:01 +0000

    health

--- app/Providers/HealthServiceProvider.php
+++ app/Providers/HealthServiceProvider.php
@@ -3,10 +3,18 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Spatie\Health\Checks\Checks\BackupsCheck;
+use Spatie\Health\Checks\Checks\DatabaseCheck;
+use Spatie\Health\Checks\Checks\DatabaseConnectionCountCheck;
+use Spatie\Health\Checks\Checks\DatabaseSizeCheck;
+use Spatie\Health\Checks\Checks\DatabaseTableSizeCheck;
 use Spatie\Health\Checks\Checks\DebugModeCheck;
 use Spatie\Health\Checks\Checks\EnvironmentCheck;
 use Spatie\Health\Checks\Checks\OptimizedAppCheck;
+use Spatie\Health\Checks\Checks\PingCheck;
+use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
 use Spatie\Health\Facades\Health;
+
 
 
 class HealthServiceProvider extends ServiceProvider
@@ -26,8 +34,33 @@ class HealthServiceProvider extends ServiceProvider
     {
         Health::checks([
             EnvironmentCheck::new(),
+
             DebugModeCheck::new(),
-            OptimizedAppCheck::new(),
+
+            OptimizedAppCheck::new()
+                ->checkConfig()
+                ->checkRoutes(),
+
+            UsedDiskSpaceCheck::new()
+                ->warnWhenUsedSpaceIsAbovePercentage(50)
+                ->failWhenUsedSpaceIsAbovePercentage(80),
+
+            BackupsCheck::new(),
+
+            PingCheck::new()->url(config('app.url')),
+
+            DatabaseCheck::new(),
+
+            DatabaseConnectionCountCheck::new()
+                ->warnWhenMoreConnectionsThan(50)
+                ->failWhenMoreConnectionsThan(100),
+
+            DatabaseSizeCheck::new()
+                ->failWhenSizeAboveGb(errorThresholdGb: 5.0),
+
+            DatabaseTableSizeCheck::new()
+                ->table('users', maxSizeInMb: 1_000)
+                ->table('posts', maxSizeInMb: 2_000),
         ]);
     }
 }
