6803097cef15cf642721c098581d40d626c5607b
Reymantis <reygrobler1@gmail.com>
2025-06-15 14:47:31 +0200

    blue

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     theme: {
         extend: {
             colors: {
-                primary: colors.red,
+                primary: colors.sky,
                 gray: colors.neutral
             },
             listStyleType: {
