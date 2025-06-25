5287d9c76ca1d094aba641b63b9bb260f69a38f3
Reymnatis <reygrobler1@gmail.com>
2025-06-25 18:33:19 +0200

    footer_update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     theme: {
         extend: {
             colors: {
-                primary: colors.sky,
+                primary: colors.red,
                 gray: colors.neutral
             },
             listStyleType: {
